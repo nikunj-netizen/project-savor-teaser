ae45d46e2fa3d4fe5d3d70e1470da171ac1f84c5
nikunj-netizen <nikunj@alehar.com>
2026-03-30 17:03:29 +0530

    Remove SlideTwo, back to 4-slide deck

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import DeckShell from "@/components/deck/DeckShell";
 import SlideCover from "@/components/slides/SlideCover";
 import SlideCore from "@/components/slides/SlideCore";
-import SlideTwo from "@/components/slides/SlideTwo";
 import SlideClosing from "@/components/slides/SlideClosing";
 import SlideLegal from "@/components/slides/SlideLegal";
 
 export default function Home() {
   return (
-    <DeckShell totalSlides={5}>
+    <DeckShell totalSlides={4}>
       <SlideCover />
       <SlideCore />
-      <SlideTwo />
       <SlideClosing />
       <SlideLegal />
     </DeckShell>
