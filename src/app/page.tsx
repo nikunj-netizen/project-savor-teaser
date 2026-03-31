4a5baa83408e002f5ff6bd8bf0463326a6243147
nikunj-netizen <nikunj@alehar.com>
2026-03-31 08:52:19 +0530

    Remove SlideDark from main deck, back to 4 slides

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import DeckShell from "@/components/deck/DeckShell";
 import SlideCover from "@/components/slides/SlideCover";
 import SlideCore from "@/components/slides/SlideCore";
-import SlideDark from "@/components/slides/SlideDark";
 import SlideClosing from "@/components/slides/SlideClosing";
 import SlideLegal from "@/components/slides/SlideLegal";
 
 export default function Home() {
   return (
-    <DeckShell totalSlides={5}>
+    <DeckShell totalSlides={4}>
       <SlideCover />
       <SlideCore />
-      <SlideDark />
       <SlideClosing />
       <SlideLegal />
     </DeckShell>
